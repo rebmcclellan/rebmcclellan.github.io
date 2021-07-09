4c739d32c5b8748e8450313c36b6bbf0dd5c2817
Brianna <rebmcclellan@gmail.com>
2021-07-09 13:44:29 -0600

    initial commit

---
+++ jsbrianna.js
@@ -0,0 +1,7 @@
+function changeclass(){
+    document.getElementById("changeclass").className = "purple";
+
+}
+function changetext(){
+    document.getElementById("changetext").innerHTML = "You changed this text!";
+}
